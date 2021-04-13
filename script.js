b4ee94fc7c06906a10899756ccd1be4af46f6d71
Amanda Hoshor <hoshor.manda@gmail.com>
2021-04-12 21:22:27 -0600

    still working on it but turning something in

--- script.js
+++ script.js
@@ -2,44 +2,35 @@ const startBtn = document.getElementById('start-btn')
 const nextBtn = document.getElementById('next-btn')
 const questionBoxElement = document.getElementById('question-box')
 const questionElement = document.getElementById('question')
-const answerButtonElement = document.getElementById('answer-buttons')
-let shuffledQuestions, currentQuestions
+const answersElement = document.getElementById('answers')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
 
 startBtn.addEventListener('click', startCodeQuiz)
 
 function startCodeQuiz() {
 startBtn.classList.add('hide')
-shuffledQuestions = questions.sort(() => Math.random() - .5)
 currentQuestionIndex = 0
 questionBoxElement.classList.remove('hide')
 setNextQuestion()
 }
 
 function setNextQuestion(){
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+    showQuestion(questions[currentQuestionIndex])
+    showAnswers(answers[currentAnswersIndex])
 }
 
 function showQuestion (question) {
    questionElement.innerText = question.question
-   question.answers.forEach(answers => {
-       const button = document.createElement('button')
-       button.innerText = answers.text
-       button.classList.add('btn')
-       if (answers.correct) {
-           button.dataset.correct = answers.correct
+   answersElement.innerText = answers.answers
+       if (answer.correctAnswer) {
+           button.dataset.correct = answer.correctAnswer
        }
        button.addEventListener('click', selectAnswer)
-       answerButtonsElement.appendChild(button)
-   })
-}
+   }
 
-function resetState(){
-    nextButton.classList.add('hide')
-    while(answerButtonsElement.firstChild){
-        answerButtonElement.removeChild
-        (answerButtonElement.firstChild)
-    }
-}
+       
+
 
 
 // function setTimeout(() => {
@@ -49,12 +40,12 @@ function resetState(){
 
 function selectAnswer(x){
     const selectedButton = x.target
-    const correct = selectedButton.dataset.correct
-    setSatusClass(document.body, correct)
+    const correct = selectedButton.dataset.correctAnswer
+    setSatusClass(document.body, correctAnswer)
     Array.from(answerButtonsElement.children).forEach(button => {
-       setStatusclass(button, button.dataset.correct) 
+       setStatusclass(button, button.dataset.correctAnswer) 
     })
-    if (shuffledQuestions.length > currentQuestionIndex + 1){
+    if (questions.length > currentQuestionIndex + 1){
     nextBtn.classList.remove('hide')
     } else {
         startBtn.innerText = 'Restart'
@@ -81,36 +72,37 @@ const questions = [
 {
     question: "What brackets do you use to start an array in JavaScript?",
      
-answers: [
-     {text:'>', correct: true}, 
-     {text:'[', correct: false},
-     {text:'{', correct: false},
-],
+answers: {
+     a:'>',
+     b:'[',
+     c:'{', 
+
         
-//  correctAnswer: "b" 
+ correctAnswer: "b" 
+},
 },
 
 {
 question: "Which of the following is NOT an example of a JavaScript data type?",
 
-answers: [
+answers: {
  
-    {text:'strings', correct: false},
-    {text:'numbers', correct: false},
-    {text:'booms' , correct: true}, 
-],  
-    // correctAnswer: "c"
+    a:'strings',
+    b:'numbers', 
+    c:'booms' , 
 
+    correctAnswer: "c"
+},
 },    
 
 { 
 question: "Which of the following is a JavaScript arithmetic operator?",
-  answers: [
-    {text:'+', correct: true}, 
-    {text:'#', correct: false},
-    {text:'!', correct: false},
-  ],
-//   correctAnswer: "a"
+  answers: {
+    a:'+',  
+    b:'#', 
+    c:'!', 
+  
+  correctAnswer: "a"
+},
 },
 ]
-
