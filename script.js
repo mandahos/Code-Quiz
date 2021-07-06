a1224ffaff3fb199a66d10e612652e352efe8630
mandahos <73262787+mandahos@users.noreply.github.com>
2021-07-05 21:31:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,108 +1,164 @@
-const startBtn = document.getElementById('start-btn')
-const nextBtn = document.getElementById('next-btn')
-const questionBoxElement = document.getElementById('question-box')
-const questionElement = document.getElementById('question')
-const answersElement = document.getElementById('answers')
-const answerButtonsElement = document.getElementById('answer-buttons')
+var timerEl = document.querySelector(".time");
+var mainEl = document.querySelector(".welcome");
+var answerEl = document.querySelector(".info");
+var startBtn = document.querySelector("#start");
+var scoreBtn = document.querySelector(".highscore")
+var resultEl = document.querySelector(".result")
+var restartBtn = document.querySelector(".reset")
+var endEl = document.querySelector(".end");
+
+var number = 0;
+var correct = 0;
+var wrong = 0;
+var timeLeft = 45;
+var timeInterval;
 
 
-startBtn.addEventListener('click', startCodeQuiz)
-
-function startCodeQuiz() {
-startBtn.classList.add('hide')
-currentQuestionIndex = 0
-questionBoxElement.classList.remove('hide')
-setNextQuestion()
-}
-
-function setNextQuestion(){
-    showQuestion(questions[currentQuestionIndex])
-    showAnswers(answers[currentAnswersIndex])
-}
-
-function showQuestion (question) {
-   questionElement.innerText = question.question
-   answersElement.innerText = answers.answers
-       if (answer.correctAnswer) {
-           button.dataset.correct = answer.correctAnswer
-       }
-       button.addEventListener('click', selectAnswer)
-   }
-
-       
-
-
-
-// function setTimeout(() => {
+var questions = [
+    {
+    question: 'What brackets do you use to start an array in JavaScript?',
+         
+    guess: ['>', '[', '{'],
+                
+    correct: '[' 
+    },
     
-// }, timeout);
+    {
+    question: 'Which of the following is NOT an example of a JavaScript data type?',
+    
+    guess: ['strings','numbers','booms'], 
+    
+    correct: 'booms'
+    },
 
-
-function selectAnswer(x){
-    const selectedButton = x.target
-    const correct = selectedButton.dataset.correctAnswer
-    setSatusClass(document.body, correctAnswer)
-    Array.from(answerButtonsElement.children).forEach(button => {
-       setStatusclass(button, button.dataset.correctAnswer) 
-    })
-    if (questions.length > currentQuestionIndex + 1){
-    nextBtn.classList.remove('hide')
-    } else {
-        startBtn.innerText = 'Restart'
-        startBtn.classList.remove('hide')
-    }
-}
-
-function setStatusclass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-      element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
-    }
-}
-
-function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-
-}
-
-const questions = [
-{
-    question: "What brackets do you use to start an array in JavaScript?",
-     
-answers: {
-     a:'>',
-     b:'[',
-     c:'{', 
-
+    { 
+    question: 'Which of the following is a JavaScript arithmetic operator?',
+      
+    guess: ['+','#','!'],
         
- correctAnswer: "b" 
-},
-},
-
-{
-question: "Which of the following is NOT an example of a JavaScript data type?",
-
-answers: {
- 
-    a:'strings',
-    b:'numbers', 
-    c:'booms' , 
-
-    correctAnswer: "c"
-},
-},    
-
-{ 
-question: "Which of the following is a JavaScript arithmetic operator?",
-  answers: {
-    a:'+',  
-    b:'#', 
-    c:'!', 
-  
-  correctAnswer: "a"
-},
-},
+    correct: '+'
+    },
+    
 ]
+
+function startQuiz () {
+    countdown();
+    startBtn.remove();
+    runQuiz();
+}
+
+function countdown () {
+     timeInterval = setInterval(function() {
+        if (timeLeft > 1) {
+            timerEl.textContent = timeLeft + " seconds";
+            timeLeft--;
+        } else {
+            timerEl.textContent = "Time Expired";
+            clearInterval(timeInterval);
+            endQuiz();
+        }
+    }, 1000);
+}
+
+function runQuiz () {
+    if(number === questions.length) {
+        endQuiz();
+    } else {
+        var question = questions[number].question;
+        var guess = questions[number].guess;
+        answer = questions[number].correct;
+
+        mainEl.textContent = question;
+        answerEl.textContent = "";
+
+        for (var i = 0; i < options.length; i++) {
+            var buttonEl = document.createElement("button");
+            buttonEl.className = "btn guess";
+            buttonEl.setAttribute("button-id", [i+1]);
+            buttonEl.textContent = `${[i+1]}. ${guess[i]}`;
+            answerEl.appendChild(buttonEl);
+       }
+        number++;
+    }
+};
+
+answerGuesses = function(event) {
+    var targetEl = event.target;
+    if(targetEl.matches(".guess")) {
+        var guessEl = targetEl.getAttribute("button-id");
+        guessesCorrect(guessEl);
+    }
+};
+
+guessesCorrect = function(guessEl) {
+    if(guessEl === answer) {
+        timeLeft += 3;
+        correct++;
+        resultEl.innerText = "Correct!";
+        runQuiz();
+    } else {
+        timeLeft -= 8;
+        wrong++;
+        resultEl.innerText = "Wrong!";
+        runQuiz();
+    }
+};
+
+endQuiz = function() {
+    resultEl.remove();
+    clearInterval(timeInterval);
+    mainEl.textContent = "Lets review your guess";
+    answerEl.textContent = " You anwered " + correct + " correct and " + wrong + "incorrectly";
+    if (correct < score) {
+        endEl.textContent = "Try again, you could get the high score next!";
+    }
+    else {
+        var input = document.createElement('input');
+        input.setAttribute('name', 'initals')
+        input.setAttribute('type', 'text');
+        input.setAttribute('placeholder', 'Enter your initals');
+        input.className  = 'input';
+        var submit = document.createElement('button');
+        submit.className = 'submit';
+        submit.setAttribute('type', 'submit');
+        submit.textContent = 'Submit';
+        endEl.appendChild(input);
+        endEl.appendChild(submit);
+    }
+}
+
+saveScore = function(event) {
+    event.preventDefault();
+    var target = event.target;
+    if(target.matches('.submit')) {
+        var form = document.querySelector('.input');
+        var playInitals = form.nodeValue;
+        if(!playInitals) {
+            alert ('Please show off your skills');
+            return;
+        }
+        else {
+            localStorage.setItem('initals', playInitals);
+            localStorage.setItem('score', correct)
+        }
+    }
+}
+
+var score = localStorage.getItem('score');
+var initals = localStorage.getItem('initals');
+function highScores() {
+    if (!score || !initals) {
+        alert('Play to add a high score');
+        return;
+    }
+    else {
+        alert('Highscore goes to' + initals + 'with a score of' + score + '!');
+    }
+}
+
+startBtn.addEventListener("click", startQuiz);
+restartBtn.addEventListener('click', startQuiz);
+scoreBtn.addEventListener('click', highScores);
+answerEl.addEventListener('click', answer)
+endEl.addEventListener("click", saveScore);
+
